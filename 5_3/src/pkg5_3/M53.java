e36ed593f21b6d9601fc78366501d54a948d3af5
jdominguezdominguez <jdominguezdominguez@victoria28.danielcastelao.org>
2017-11-06 09:23:03 +0100

    Ejercicio 5_3 terminado

---
+++ 5_3/src/pkg5_3/M53.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg5_3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jdominguezdominguez
+ */
+public class M53 {
+    Scanner sc=new Scanner(System.in);
+    public int setNum(){
+        System.out.println("Introduzca un numero");
+        int n=sc.nextInt();
+        return n;
+    }
+    
+}
+    
