2a6c05865da0ff795f2d44d01198806d7dc92465
jdominguezdominguez <jdominguezdominguez@victoria28.danielcastelao.org>
2017-11-06 09:26:08 +0100

    Ejercicio 5_5 terminado

---
+++ 5_5/src/pkg5_5/M55.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg5_5;
+import java.util.Scanner;
+/**
+ *
+ * @author jdominguezdominguez
+ */
+public class M55 {
+    Scanner sc=new Scanner(System.in);
+    public int setNum(){
+        System.out.println("Introduzca un numero");
+        int num=sc.nextInt();
+        return num;
+    }
+}
