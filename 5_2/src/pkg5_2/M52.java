1fa4b5cdee231ba63795818a796a83a4233b5695
jdominguezdominguez <jdominguezdominguez@victoria28.danielcastelao.org>
2017-11-06 09:22:05 +0100

    Ejercicio 5_2 terminado

---
+++ 5_2/src/pkg5_2/M52.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg5_2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jdominguezdominguez
+ */
+public class M52 {
+    Scanner sc=new Scanner(System.in);
+    public short setNum1(){
+        System.out.println("Introduzca el primer numero");
+        short a=sc.nextShort();
+        return a;
+    }
+    public short setNum2(){
+        System.out.println("Introduzca el segundo numero");
+        short b=sc.nextShort();
+        return b;
+    }
+}
